1aaa0298f45a7cf1f0f7ddadc2bae0423c95ae26
ma-he-sh <mi6softlab@gmail.com>
2021-03-02 01:27:27 -0500

    --init--

---
+++ server/app/server.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, session, render_template, redirect, request, g, jsonify
+import os 
+import sys
+
+app = Flask(__name__)
+
+#index page 
+@app.route('/')
+def index():
+    return 'test'
+
+#404
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)

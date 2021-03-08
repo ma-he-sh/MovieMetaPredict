9274befe0b7b221fae5332d7e4b0237c3f65205d
Mahesh Ranaweera <mi6softlab@gmail.com>
2021-03-07 20:34:45 -0500

    Sample Paylaod
    Sample Paylaod

--- server/server.py
+++ server/server.py
@@ -32,9 +32,66 @@ def index():
 #get title
 @app.route(f"/{NAMESPACE}/title/<movietitle>", methods=["GET"])
 def get_title(movietitle):
+    sample_payload = {
+        "id": "tt0068646",
+        "href": "https://imdb.com/title/tt0068646",
+        "title": "The Godfather",
+        "year": "1972",
+        "meta": {
+            "certificate": "18A",
+            "runtime": "175 min",
+            "genre": ["Crime", "Drama"],
+            "meta_score": "100",
+            "description": "An organized crime ...",
+            "directors": [
+                "Francis Ford Coppola"
+            ],
+            "votes": "1,628,276",
+            "gross": "$134.97M",
+            "awards": {
+                "Actors": "5 Stars",
+                "Direction": "5 Stars",
+                "Screenplay": "5 Stars",
+                "Oscars": "3",
+                "Oscar Nominations": "11",
+                "BAFTA Awards": "0",
+                "BAFTA Nominations": "4",
+                "Golden Globes": "6",
+                "Golden Globe Nominations": "8"
+            },
+            "cast": [
+                {
+                    "actor": "Marlon Brando",
+                    "actor_link": "/name/nm0000008/",
+                    "character": "Don Vito Corleone",
+                    "character_link": "/title/tt0068646/characters/nm0000008"
+                },
+                ...
+            ],
+            "Country:": ["USA"],
+            "Language:": ["English","Italian","Latin"],
+            "Release Date:": "24 March 1972 (Canada)",
+            "Also Known As:": "Le parrain",
+            "Filming Locations:": [
+                "NY Eye and Ear Infirmary, 2nd Avenue & East 13th Street, New York City, New York, USA"
+            ],
+            "Budget:": "$6,000,000            (estimated)",
+            "Opening Weekend USA:": "$302,393,19 March 1972",
+            "Gross USA:": "$134,966,411",
+            "Cumulative Worldwide Gross:": "$246,120,986",
+            "Production Co:": ["Paramount Pictures", "Alfran Productions"],
+            "Runtime:": "175 min",
+            "Sound Mix:": ["DTS", "Mono"],
+            "Color:": ["Color"],
+            "Aspect Ratio:": "1.85 : 1",
+            "storyline": "The Godfather ....",
+            "rating": "18A"
+        }
+    }
+    
     payload = {
         'title' : movietitle,
-        'payload': {}
+        'payload': sample_payload,
     }
     return json_response( callback=payload )
 
